9bdf93ba1ec5ce9f413f3159b4326c4120c7c184
Emilio Haro <ermsharo@gmail.com>
2023-02-10 14:53:11 -0300

    add redux stuff

--- my-app/src/components/CreateUserForm.tsx
+++ my-app/src/components/CreateUserForm.tsx
@@ -1,9 +1,16 @@
 import { Input, Label, Box, Button } from "./../styles/generalStyles";
 
 export default function CreateUserForm() {
+  //Props
+  //type
+  //name
+  //value
+  //handlechange
+  //label
   return (
     <Box>
       <h2>Cadastrar usuario</h2>
+
       <Label>Nome</Label>
       <Input type="text" placeholder="Full name" required />
       <Label>Email</Label>
