2c0d31dc634cbf46e3a1d7425e35847ae87a76b3
Emilio Haro <ermsharo@gmail.com>
2023-02-13 13:07:36 -0300

    update or changes

--- my-app/src/services/requestUsers.ts
+++ my-app/src/services/requestUsers.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useAppDispatch } from "../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { User } from "../types";
+import {
+  isMailValid,
+  thisEmailExist,
+  thisIdExist,
+  thisUsernameExist,
+} from "../utils/formValidations";
 import { generateNewID } from "../utils/userListUtils";
 import { addNewUser } from "./../redux/features/user/userSlice";
-
-
-
-
 
 export const RequestUsers = () => {
   const [data, setData] = useState<any>(null);
@@ -14,27 +17,56 @@ export const RequestUsers = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const dispatch = useAppDispatch();
 
-  const sendUser = (item:any, index:number)=> {
-    // console.log("Usuario saido da api", item)
-     const userDefaultObj = {
-         id: generateNewID(),
-         username: `${item.name}`,
-         email: `${item.email}`,
-         status: true,
-       };
-       dispatch(addNewUser(userDefaultObj));
+  const userList = useAppSelector((state) => state.user.users);
 
-  }
-  
-  const runUserData = (data: any) =>{
-    console.log("user data",data)
-  data.forEach(sendUser);
-  }
-  
+  const verifyErrors = (username: string, email: string) => {
+    let formErrors = [];
+    if (username === "") {
+      formErrors.push("Campo nome é obrigatorio");
+    }
 
+    if (thisUsernameExist(userList, username)) {
+      formErrors.push("Nome de usuário já existe");
+    }
+
+    if (thisEmailExist(userList, email)) {
+      formErrors.push("Email já cadastrado");
+    }
+    if (!isMailValid(email)) {
+      formErrors.push("Email invalido");
+    }
+
+    if (formErrors.length === 0) {
+      let id = generateNewID();
+      while (thisIdExist(userList, id)) {
+        id = generateNewID();
+      }
+      const userDefaultObj = {
+        username: `${username}`,
+        email: `${email}`,
+        id: id,
+        status: true,
+      };
+      dispatch(addNewUser(userDefaultObj));
+    }
+  };
+
+  const sendUser = (item: any, index: number) => {
+
+    const userDefaultObj = {
+      id: generateNewID(),
+      username: `${item.name}`,
+      email: `${item.email}`,
+      status: true,
+    };
+    dispatch(addNewUser(userDefaultObj));
+  };
+
+  const runUserData = (data: any) => {
+    data.forEach(sendUser);
+  };
 
   const fetchData = async (): Promise<void> => {
-    
     setLoading(true);
     setError(false);
     try {
@@ -43,8 +75,6 @@ export const RequestUsers = () => {
       );
       setData(response.data);
       runUserData(response.data);
-  
-
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error("Axios Error with Message: " + error.message);
